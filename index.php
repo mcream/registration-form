b8a613882a88b0cc5cb403b0232167f2875c1748
travis <travis@travis-ci.org>
2019-04-22 18:00:47 +0200

    Added register template in main file Assigning ID names.

--- index.php
+++ index.php
@@ -4,7 +4,25 @@
     <meta charset="utf-8">
     <title>Logowanie</title>
   </head>
+  <style>
+  .container{
+    width: 190px;
+    display: block;
+    float: left;
+  }
+  </style>
   <body>
+    <div class="container">
+      <form class="" action="index.html" method="post">
+        Login: <input type="text" name="login" value="">
+        Hasło: <input type="text" name="password" value="">
+        re-Hasło: <input type="text" name="repassword" value="">
+        Regulamin: <input type="text" name="rules" value="">
+        <input type="submit" name="" value="submit">
+      </form>
+    </div>
+    <div class="content">
 
+    </div>
   </body>
 </html>

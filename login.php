ca17b58523c9ceea4195caeaa64fb9dff30be7f6
travis <travis@travis-ci.org>
2019-04-23 00:02:01 +0200

    Update template login

---
+++ login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Logowanie</title>
+  </head>
+  <style>
+  .container{
+    width: 190px;
+    display: block;
+    float: left;
+  }
+  input[type='submit']{
+    width: 120px;
+  }
+  </style>
+  <body>
+    <div class="container">
+      <form class="" action="register.php" method="post">
+        Login: <input type="text" name="login" value="">
+        Hasło: <input type="password" name="password" value="">
+        <input type="submit" name="" value="submit">
+      </form>
+    </div>
+    <div class="content">
+
+    </div>
+  </body>
+</html>

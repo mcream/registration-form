ddc9d682b7fc212443034ddd8fc044ca0f819707
travis <travis@travis-ci.org>
2019-04-22 23:56:45 +0200

    Update types in inupts at index.php

--- index.php
+++ index.php
@@ -15,8 +15,8 @@
     <div class="container">
       <form class="" action="index.html" method="post">
         Login: <input type="text" name="login" value="">
-        Hasło: <input type="text" name="password" value="">
-        re-Hasło: <input type="text" name="repassword" value="">
+        Hasło: <input type="password" name="password" value="">
+        re-Hasło: <input type="password" name="repassword" value="">
         Regulamin: <input type="text" name="rules" value="">
         <input type="submit" name="" value="submit">
       </form>
